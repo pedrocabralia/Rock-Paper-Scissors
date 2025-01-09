8895794c7c8a06fd6066fccf9e8a9b5ba04565bf
pedrocabralia <pedrocabralia@hotmail.com>
2025-01-09 14:40:38 -0300

    pontos

---
+++ aFazer.md
@@ -0,0 +1,4 @@
+# A FAZER
+( ) Exiba a pontuação atual e anuncie o vencedor do jogo quando um jogador atingir 5 pontos.
+(  )Mesclar com a main
+(  ) Remover o branch secundário
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let humanScore = 0;
 let computerScore = 0;
 let valorEscolhaHumano = 0;
 let valorEscolhaRobo = 0;
+const acabarJogo = document.getElementsByClassName("sumir")[0];
+
+const acabarJogo2 = document.getElementsByClassName("sumir")[1];
+
 
 function getHumanChoice() {
     const pedra = document.getElementById("pedra");
@@ -51,21 +55,57 @@ function getRoboChoice() {
 }
 
 function verificarVencedor() {
+    
     if (valorEscolhaHumano === valorEscolhaRobo) {
+
+        const resultado = document.getElementById("vencedor");
+        const resultadoVencedor = document.createElement("h3");
+        resultadoVencedor.textContent = "Empate";
+        resultado.appendChild(resultadoVencedor);
         
     } else if (
         (valorEscolhaHumano === 1 && valorEscolhaRobo === 3) || // Pedra vence Tesoura
         (valorEscolhaHumano === 2 && valorEscolhaRobo === 1) || // Papel vence Pedra
         (valorEscolhaHumano === 3 && valorEscolhaRobo === 2)    // Tesoura vence Papel
     ) {
-       
+        const resultado = document.getElementById("vencedor");
+        const resultadoVencedor = document.createElement("h3");
+        resultadoVencedor.textContent = "Vitória Humano";
+        resultado.appendChild(resultadoVencedor);
         humanScore++;
+
     } else {
         
+
+        const resultado = document.getElementById("vencedor");
+        const resultadoVencedor = document.createElement("h3");
+        resultadoVencedor.textContent = "Vitória Robô";
+        resultado.appendChild(resultadoVencedor);
         computerScore++;
     }
+ 
+     console.log(humanScore);
+     console.log(computerScore);
+     const pontosHumano = document.getElementById("pontuacaoHumano");
+     pontosHumano.innerText = humanScore;
+     const pontosRobo = document.getElementById("pontuacaoRobo");
+     pontosRobo.innerText = computerScore;
 
-    
+
+     if(humanScore == 5 || computerScore ==5){
+         
+        console.log(humanScore)
+        console.log(computerScore)
+        
+
+         acabarJogo.style.display = "none";
+         acabarJogo2.style.display = "none";
+
+
+     }
+
+
+
 }
 
 function limparEscolha(elementId) {
@@ -77,7 +117,6 @@ function limparEscolha(elementId) {
 }
 
 
-
 function reiniciar() {
     const botaoReiniciar = document.getElementById("reiniciar-jogo");
     botaoReiniciar.addEventListener("click", () => {
@@ -87,3 +126,4 @@ function reiniciar() {
 
 getHumanChoice();
 reiniciar();
+
